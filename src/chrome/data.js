e95acb9f15285a35bf1537342bf60dc20140a9bd
kongliyuan <kongliyuan@360.cn>
2022-05-07 19:14:14 +0800

    feat

---
+++ src/chrome/data.js
@@ -0,0 +1,63 @@
+import { storage } from './storage';
+import { parentClient, ChromeMessage } from './message';
+
+function getCookie(url) {
+    return new Promise((resolve) => {
+        chrome.cookies.getAll({ url }, (cookies) => {
+            resolve(cookies);
+        });
+    });
+}
+
+function setCookie(val) {
+    chrome.cookies.set(val, () => {
+        console.log('set cookie error', chrome.extension.lastError);
+        console.log(chrome.runtime.lastError);
+    });
+}
+
+function removeCookie() {
+    chrome.cookies.remove();
+}
+
+async function setData(name, val = true, key = 'users') {
+    const data = await storage.get(key) || {};
+    data[name] = {...(data[name] || {}),...val};
+    storage.set(key, data);
+}
+
+async function removeData(name, key = 'users') {
+    const data = await storage.get(key) || {};
+    delete data[name];
+    storage.set(key, data);
+}
+
+async function toggleUser({ name, domain: currentPageDomain, url }) {
+    const data = await storage.get('users') || {};
+    const detail = data[name];
+    const cookie = detail.cookie || [];
+    const parentCookie = [];
+    storage.set('activeUser',name)
+    cookie.forEach(({ hostOnly, session, ...item }) => {
+        const { domain } = item;
+        item.url = url;
+        const isParentDomain = domain !== currentPageDomain;
+        if (isParentDomain) {
+            setCookie(item);
+        } else {
+            parentCookie.push(item);
+        }
+    });
+    if (parentCookie.length) {
+        parentClient.sendMessage(new ChromeMessage('set-parent-cookie', parentCookie));
+    }
+}
+
+export {
+    getCookie,
+    setCookie,
+    removeCookie,
+    setData,
+    removeData,
+    toggleUser
+};
---
+++ src/content-scripts/config.js
@@ -0,0 +1,5 @@
+export const kuaichuanPermissionConf={
+    10:'原创文章',
+    11:'视频',
+    14:'cube'
+}
